579b811c044c2268d88e1b8bb5c027786ada65ef
zhengyongkai <1101207096@qq.com>
2022-12-08 17:47:52 +0800

    V狂神说_Spring详解_P9 依赖注入之Set注入

--- spring-study/spring-04-di/src/test/java/MyTest.java
+++ spring-study/spring-04-di/src/test/java/MyTest.java
@@ -14,7 +14,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		Student student = (Student) context.getBean("student");
-		System.out.println(student);
+		System.out.println(student.toString());
 
 
 	}
