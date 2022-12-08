aa391703e4dbcd6beb979373cb2f492d8315aa8d
zhengyongkai <1101207096@qq.com>
2022-12-08 10:29:58 +0800

    V狂神说_Spring详解_P7 Spring配置说明

--- spring-study/spring-03-ioc2/src/test/java/MyTest.java
+++ spring-study/spring-03-ioc2/src/test/java/MyTest.java
@@ -1,4 +1,5 @@
 import com.zyk.pojo.User;
+import com.zyk.pojo.UserT;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -11,15 +12,23 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @Description: 总结：在配置文件加载的时候，容器中管理的对象就已经初始化了！
  **/
 public class MyTest {
-	public static void main(String[] args) {
-		/*User user = new User();
+	/*public static void main(String[] args) {
+		*//*User user = new User();
 		user.setName("郑永凯");
+		user.show();*//*
+		*//*ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		UserT user = (UserT) context.getBean("userT2");
+		user.show();*//*
+		*//*User user2 = (User) context.getBean("user");
+		System.out.println("user==user2:" + (user == user2));*//*
+	}*/
+
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		/*User user = (User) context.getBean("userT1");
 		user.show();*/
-
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		User user = (User) context.getBean("user");
-		User user2 = (User) context.getBean("user");
-		System.out.println("user==user2:" + (user == user2));
-
+		UserT user = (UserT) context.getBean("userT4");
+		user.show();
 	}
 }
