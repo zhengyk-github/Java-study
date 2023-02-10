6c030f8208540feef3f6922fa7882c8bd01d20ba
zhengyongkai <1101207096@qq.com>
2023-02-10 09:28:21 +0800

    Vspring-study 父项目包pom.xml引入json

---
+++ spring-study/src/main/java/test.java
@@ -0,0 +1,19 @@
+
+/**
+ * @ClassName: test
+ * @Date: Created in 2023/1/10 14:26
+ * @Version: 1.0
+ * @Author: zhengyongkai
+ * @Modified By:
+ * @Description: TODO
+ **/
+public class test {
+
+	public static void main(String[] args) {
+		Integer x = new Integer(1);
+		Integer y = new Integer(1);
+		System.out.println(x.equals(y)); // true
+		System.out.println(x == y);      // false
+
+	}
+}
