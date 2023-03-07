925acd14c6082157def421fe5316cfad191b66ee
zhengyongkai <1101207096@qq.com>
2023-03-07 14:45:08 +0800

    Vspringcloud P6-Eureka：什么是Eureka

---
+++ springcloud/springcloud-eureka-7001/src/main/java/com/zyk/springcloud/EurekaServer_7001.java
@@ -0,0 +1,23 @@
+package com.zyk.springcloud;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName: EurekaServer_7001
+ * @Date: Created in 2023/3/7 14:37
+ * @Version: 1.0
+ * @Author: zhengyongkai
+ * @Modified By:
+ * @Description: TODO
+ **/
+@SpringBootApplication
+@EnableEurekaServer //@EnableEurekaServer 服务端的启动类，可以接受别人注册进来
+public class EurekaServer_7001 {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServer_7001.class,args);
+	}
+	//启动之后访问 http://localhost:7001/
+}
